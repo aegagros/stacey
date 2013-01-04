25276c2573e1652e592478f341774166fc94d4db
aegagros <manolis_paterakis@yahoo.g>
2013-01-05 01:01:55 +0200

    Added documentation to config.php

--- extensions/config.php
+++ extensions/config.php
@@ -8,12 +8,20 @@ class Config {
   public static $templates_folder = './templates';
   public static $cache_folder = './app/_cache';
   public static $extensions_folder = './extensions';
+  /*
+   * Add desired available languages here as follows
+   *  'lang-code' => 'lang-name',
+   *
+   * 'lang-code' is used for path prefixes and text file lookup
+   * 'lang-name' is used for language display name
+   */
   public static $languages = array(
-    'default' => 'en',
     'available' => array(
       'en' => 'English',
       'el' => 'Ελληνικά',
     ),
+    # replace this with one of the above lang-codes to set default language, when none is specified in the url
+    'default' => 'en',
   );
 
 }
